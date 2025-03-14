b8b159e238274345d78bfa104811ebfd54d35639
George Johnson <george@aceworks.com>
2025-03-14 16:44:09 +0000

    Clean cache and update dependencies

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,9 @@
 // pages/index.js
-import { useEffect } from "react";
-import { useRouter } from "next/router";
-
 export default function Home() {
-  const router = useRouter();
-  useEffect(() => {
-    router.push("/login");
-  }, []);
-  return null;
-}
+    return (
+      <div>
+        <h1>Home Page</h1>
+        <p>Please visit <a href="/login">/login</a> to log in.</p>
+      </div>
+    );
+  }
