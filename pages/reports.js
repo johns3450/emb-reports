8523bd7b89f5e0e89691ea7473b2fdead857765f
George Johnson <george@aceworks.com>
2025-03-14 16:31:59 +0000

    Clean cache and update dependencies

--- pages/reports.js
+++ pages/reports.js
@@ -8,9 +8,8 @@ export default function Reports() {
 
   return (
     <div>
-      <h1>Reports Dashboard</h1>
       {/* Embed your Data Studio reports or other content here */}
-      <iframe src="https://lookerstudio.google.com/embed/reporting/0dc53cbc-caa0-4e14-b3a4-25d05c94dd1c/page/c8RBF" width="100%" height="600"></iframe>
+      <iframe src="https://lookerstudio.google.com/embed/reporting/0dc53cbc-caa0-4e14-b3a4-25d05c94dd1c/page/c8RBF" width="1200" height="900"></iframe>
     </div>
   );
 }
